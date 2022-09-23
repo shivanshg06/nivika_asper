9bc728921630ea45a071f8a81af56f84b6b622ff
EAM2050 <munshiakhileshwer@gmail.com>
2022-09-24 00:16:02 +0530

    By AM 4

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -13,122 +13,129 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Text('This is settings screen'),
-          Column(
-            children: [
-              ExpansionTile(
-                leading: Icon(
-                  Icons.translate,
-                  size: 30,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'This is settings screen',
+              style: TextStyle(fontSize: 20),
+            ),
+            Column(
+              children: [
+                ExpansionTile(
+                  leading: Icon(
+                    Icons.translate,
+                    size: 30,
+                  ),
+                  title: Text(
+                    'Choose Language',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  children: [
+                    ReusableListTile(
+                      title: Text(
+                        'English',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Hindi',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Assamese',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Bengali',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Gujarati',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Marathi',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Tamil',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                    ReusableListTile(
+                      title: Text(
+                        'Telugu',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      onTap: () {},
+                    ),
+                  ],
                 ),
-                title: Text(
-                  'Choose Language',
-                  style: TextStyle(fontSize: 18),
-                ),
-                children: [
-                  ReusableListTile(
+                ReusableListTile(
                     title: Text(
-                      'English',
-                      style: TextStyle(fontSize: 15),
+                      'Contact Us',
+                      style: TextStyle(fontSize: 18),
                     ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
+                    listIcon: Icon(
+                      Icons.connect_without_contact,
+                      size: 30,
+                    ),
+                    onTap: () {}),
+                ReusableListTile(
                     title: Text(
-                      'Hindi',
-                      style: TextStyle(fontSize: 15),
+                      'Help Centre',
+                      style: TextStyle(fontSize: 18),
                     ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
+                    listIcon: Icon(
+                      Icons.contact_support_outlined,
+                      size: 30,
+                    ),
+                    onTap: () {}),
+                ReusableListTile(
                     title: Text(
-                      'Assamese',
-                      style: TextStyle(fontSize: 15),
+                      'Legal Statements',
+                      style: TextStyle(fontSize: 18),
                     ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
+                    listIcon: Icon(
+                      Icons.gavel_rounded,
+                      size: 30,
+                    ),
+                    onTap: () {}),
+                ReusableListTile(
                     title: Text(
-                      'Bengali',
-                      style: TextStyle(fontSize: 15),
+                      'About Us',
+                      style: TextStyle(fontSize: 18),
                     ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
-                    title: Text(
-                      'Gujarati',
-                      style: TextStyle(fontSize: 15),
+                    listIcon: Icon(
+                      Icons.info_outline_rounded,
+                      size: 30,
                     ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
-                    title: Text(
-                      'Marathi',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
-                    title: Text(
-                      'Tamil',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    onTap: () {},
-                  ),
-                  ReusableListTile(
-                    title: Text(
-                      'Telugu',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    onTap: () {},
-                  ),
-                ],
-              ),
-              ReusableListTile(
-                  title: Text(
-                    'Contact Us',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  listIcon: Icon(
-                    Icons.connect_without_contact,
-                    size: 30,
-                  ),
-                  onTap: () {}),
-              ReusableListTile(
-                  title: Text(
-                    'Help Centre',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  listIcon: Icon(
-                    Icons.contact_support_outlined,
-                    size: 30,
-                  ),
-                  onTap: () {}),
-              ReusableListTile(
-                  title: Text(
-                    'Legal Statements',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  listIcon: Icon(
-                    Icons.gavel_rounded,
-                    size: 30,
-                  ),
-                  onTap: () {}),
-              ReusableListTile(
-                  title: Text(
-                    'About Us',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  listIcon: Icon(
-                    Icons.info_outline_rounded,
-                    size: 30,
-                  ),
-                  onTap: () {}),
-            ],
-          )
-        ],
+                    onTap: () {}),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
